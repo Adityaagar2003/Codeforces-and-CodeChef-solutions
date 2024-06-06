b64fabe870ff415706c03e6e85484c4089ff3be4
Aditya Agarwal <136118380+Adityaagar2003@users.noreply.github.com>
2024-06-06 21:47:41 +0530

    Add files via upload

---
+++ Z_Hard_Compare.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main()  {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int a,b,c,d;   cin >> a>>b>>c>>d;
+    int p1=pow(a,b);
+    int p2=pow(c,d);
+    if(a==c&&b==d){
+        cout<<"NO"<<endl;
+
+    }
+    else if(p1>p2){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+        
+    }
+   
+}
+
+
